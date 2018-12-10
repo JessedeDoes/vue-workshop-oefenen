36a6bb02d6d52c0004ee5800bbbae31876a95dd0
jessededoes <jesse.dedoes@inl.nl>
2018-12-10 16:20:50 +0100

    .

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,6 +5,8 @@
         <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
         <button @click="decrement">-</button>
         <button @click="increment">+</button>
+        <div id="kaartje" style="with:100%; height: 1000px"></div>
+        
         <table border="border" id="example-1">
             <tr>
                 <td v-for="field in header"><b>{{field}}</b></td>
@@ -12,7 +14,8 @@
             <tr v-for="result in results">
                 <td v-for="f in Object.keys(result)"><span :class="f"> {{result[f]}}</span></td>
         </tr>
-</table>
+    </table>
+    
     </div>
     
 </template>
@@ -20,6 +23,31 @@
 <script lang="ts">
 import Vue from "vue";
 import axios from "axios";
+import * as L from "leaflet";
+//import 'ol/ol.css';
+
+import VueLayers from 'vuelayers'
+//import 'vuelayers/lib/style.css' // needs css-loader
+
+Vue.use(VueLayers)
+
+// or individual components
+
+// kijk ook eens naar: https://gist.github.com/bertt/4956504
+
+import { Map, TileLayer, OsmSource, Geoloc} from 'vuelayers'
+//import 'vuelayers/lib/style.css' // needs css-loader
+//Vue.use(XYZSource)
+//Vue.use(View)
+Vue.use(Map)
+Vue.use(TileLayer)
+Vue.use(OsmSource)
+Vue.use(Geoloc)
+
+//import Map f,rom 'ol/Map';
+//import View from 'ol/View';
+//import TileLayer from 'ol/layer/Tile';
+//import XYZSource from 'ol/source/XYZ';
 
 const testje = function(x: string): string { return `http://localhost:8080/hercules/DSDD?query=select * from integratie.keywords where lemma='${x}' or keyword='${x}'` }
 const testje1 = function(x: string): string { return `http://localhost:8080/hercules/DSDD?query=select max(keywords.lemma) as lemma, 
@@ -71,6 +99,7 @@ export default Vue.extend({
                 this.results = result.data.results;
                 alert(JSON.stringify(this.results))
                 alert(JSON.stringify([this.ulx, this.uly, this.lrx, this.lry]))
+                alert(this.kaartje)
             }, error => {
                 console.error(error);
             });
@@ -95,6 +124,25 @@ export default Vue.extend({
         lrx(): number { var iksen:Array<number> = this.allPoints.map((x: [number,number]) => x[0]); return max(iksen) },
         uly(): number { var yen:Array<number> = this.allPoints.map((x: [number,number]) => x[1]); return min(yen) },
         lry(): number { var yen:Array<number> = this.allPoints.map((x: [number,number]) => x[1]); return max(yen) },
+        kaartje(): L.Map {
+            //var map = new L.Map("kaartje");
+//map.setView(new L.LatLng(51.505, -0.09), 13);
+//var layer =new L.TileLayer("http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png", { maxZoom: 18, attribution: "attribution test" })
+//layer.addTo(map);
+            //return map;
+            	// set up the map
+	var map = new L.Map('kaartje');
+
+	// create the tile layer with correct attribution
+	var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(51.3, 0.7),9);
+    map.addLayer(osm);
+    return map;
+        }
         //bbox(): Array<[number, number]> = { return [[.1,.2]]}
         //bbox(): ((number,number), (number,number)) =  { return ((this.ulx, this.uly), (this.lrx, this.lry)) }
     }
@@ -102,5 +150,10 @@ export default Vue.extend({
 </script>
 
 
+<!--
+<style lang="scss">
+.greeting {font-size: 20px;}
+</style>
+-->
 
 
--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,5 +1,8 @@
 // src/vue-shims.d.ts
 
+declare module 'ol';
+declare module 'vuelayers';
+
 declare module "*.vue" {
     import Vue from "vue";
     export default Vue;
